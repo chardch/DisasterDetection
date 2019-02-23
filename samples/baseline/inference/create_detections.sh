787fa48e147d2f63f2d36381b60fd0fedb95d59f
Richard Chen <chardch@google.com>
2019-02-22 18:39:50 -0800

    add utils for training obj det api

--- samples/baseline/inference/create_detections.sh
+++ samples/baseline/inference/create_detections.sh
@@ -4,5 +4,5 @@ TESTLIST="christchurch_519_14.jpg christchurch_519_15.jpg"
 
 for i in $TESTLIST; do
     echo $i
-    python3 create_detections.py -c ../model/multires.pb -o 'preds_output/'$i'.txt' '/host/DisasterDetection/datasets/AIRS/train/image/'$i
+    python3 create_detections.py -c ../exported_transfer_v2/frozen_inference_graph.pb -o 'preds_output/'$i'.txt' '/host/datasets/AIRS/trainval/train/image_jpg_split/'$i
 done
